ee9f0195f98ac75f5c0d130ed6de44b805bea5a0
Michelle Cordeiro <mic321@gmail.com>
2025-02-21 18:16:55 -0300

    feat: add invite page

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,11 +1,18 @@
 import type { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 interface IconButtonProps extends ComponentProps<'button'> {}
 
-export function IconButton(props: IconButtonProps) {
+export function IconButton({ className, ...props }: IconButtonProps) {
   return (
     <button
-      className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-200 hover:bg-blue hover:text-gray-900"
+      //twMerge -> add new property or subscribe
+      //without it all previous classname will be replaced
+      //when IconButton is used passing some property it will be added to the existing ones or subscribe to some
+      className={twMerge(
+        'p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-200 hover:bg-blue hover:text-gray-900',
+        className
+      )}
       {...props}
     />
   )
