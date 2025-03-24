0c428f2958a96e302fb67da215e340fe18dbb956
Michelle Cordeiro <mic321@gmail.com>
2025-03-24 18:12:30 -0300

    feat: implement stats and ranking

--- src/app/invite/[subscriberId]/ranking.tsx
+++ src/app/invite/[subscriberId]/ranking.tsx
@@ -1,9 +1,12 @@
+import { getRanking } from '@/http/api'
 import Image from 'next/image'
 import cooper from '../../../assets/medal-cooper.svg'
 import gold from '../../../assets/medal-gold.svg'
 import silver from '../../../assets/medal-silver.svg'
 
-export function Ranking() {
+export async function Ranking() {
+  const { ranking } = await getRanking()
+
   return (
     <div className="w-full max-w-[440px] space-y-5">
       <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
@@ -11,42 +14,48 @@ export function Ranking() {
       </h2>
 
       <div className="space-y-4">
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">1º</span> | Michelle Cordeiro
-          </span>
-          <span className="font-heading font-semibold text-2xl text-gray-200 leading-none">
-            853
-          </span>
+        {ranking.map((item, index) => {
+          const rankingPosition = index + 1
 
-          <Image src={gold} alt="medal" className="absolute top-0 right-8" />
-        </div>
-      </div>
+          return (
+            <div
+              key={item.id}
+              className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3"
+            >
+              <span className="text-sm text-gray-300 leading-none">
+                <span className="font-semibold">{rankingPosition}º</span> |{' '}
+                {item.name}
+              </span>
+              <span className="font-heading font-semibold text-2xl text-gray-200 leading-none">
+                {item.score}
+              </span>
 
-      <div className="space-y-4">
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">2º</span> | Mic Cordeiro
-          </span>
-          <span className="font-heading font-semibold text-2xl text-gray-200 leading-none">
-            79
-          </span>
+              {rankingPosition === 1 && (
+                <Image
+                  src={gold}
+                  alt="medal"
+                  className="absolute top-0 right-8"
+                />
+              )}
 
-          <Image src={silver} alt="medal" className="absolute top-0 right-8" />
-        </div>
-      </div>
+              {rankingPosition === 2 && (
+                <Image
+                  src={silver}
+                  alt="medal"
+                  className="absolute top-0 right-8"
+                />
+              )}
 
-      <div className="space-y-4">
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">3º</span> | Mi Cord
-          </span>
-          <span className="font-heading font-semibold text-2xl text-gray-200 leading-none">
-            32
-          </span>
-
-          <Image src={cooper} alt="medal" className="absolute top-0 right-8" />
-        </div>
+              {rankingPosition === 3 && (
+                <Image
+                  src={cooper}
+                  alt="medal"
+                  className="absolute top-0 right-8"
+                />
+              )}
+            </div>
+          )
+        })}
       </div>
     </div>
   )
